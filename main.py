0e0002e80141b352a6b64cf6370fc63ff692fb56
Tw0side <twoside2004@gmail.com>
2025-01-11 12:55:20 +0530

    Rough frontend framework

--- main.py
+++ main.py
@@ -81,6 +81,17 @@ if selected=="Real Time Analysis":
         #pass the values from the filter process to the dataframe to dynamically filter the dataframe
         st.sidebar.success("Filtering successfull")
 
+    st.sidebar.header("Set Threshold for packet counts")
+    datatime=st.sidebar.slider("Select the Time interval To calulate mean",1,60)
+    pakcetcount=st.sidebar.slider("Select the number of pakcets allowed in this timeframe",1)
+    if st.sidebar.button("Apply The settings"):
+        success_placeholder = st.sidebar.empty()
+        success_placeholder.success("Threshold Applied Successfully!")
+            
+            # Wait for 2 seconds before clearing the message
+        time.sleep(2)
+        success_placeholder.empty()
+
 #PURELY FOR DEMO PURPOSE MAPS
     # Create a Folium map
     m = folium.Map(location=[37.7749, -122.4194], zoom_start=5)
@@ -109,26 +120,65 @@ if selected == "Historical data analysis":
 
     if selection == "View Data From Today":
         current_time=datetime.datetime.now().time()
-        max_time_today = (datetime.datetime.combine(datetime.date.today(), current_time) - datetime.timedelta(hours=1)).time()
+        max_time_today = (datetime.datetime.combine(datetime.date.today(), current_time))
 
 
-        selected_Time=st.sidebar.time_input(
+        Start_Time=st.sidebar.time_input(
             "Provide The Start Time",
             value=datetime.time(12,0)
             )
-        if selected_Time > max_time_today:
+        if Start_Time > current_time:
             st.sidebar.warning("Please choose the Time from the allowed Range")
         else:
             today=datetime.date.today()
-            selected_datetime_today = datetime.datetime.combine(today,selected_Time)
+            selected_datetime_today = datetime.datetime.combine(today,Start_Time)
 
-    if st.sidebar.button("Fetch Data"):
-        success_placeholder = st.sidebar.empty()
-        success_placeholder.success("Filters Applied Successfully!")
+        Stop_Time=st.sidebar.time_input(
+            "Provide The Stop Time",
+            value=datetime.time(12,0)
+            )
+        if Stop_Time < Start_Time:
+            st.sidebar.warning("Stopping time cannot be earlier than starting")
+        else:
+            today=datetime.date.today()
+            selected_datetime_today = datetime.datetime.combine(today,Stop_Time)
+
+        if st.sidebar.button("Fetch Data"):
+            #Fetch data from the database where the date is the date of the day and the timeframe is between Start_Time and Stop_Time
+            success_placeholder = st.sidebar.empty()
+            success_placeholder.success("Filters Applied Successfully!")
+            
+            # Wait for 2 seconds before clearing the message
+            time.sleep(2)
+            success_placeholder.empty()
+
+#Logic of Data from current day ends.
+    elif selection == "Data From Another Day":
         
-        # Wait for 2 seconds before clearing the message
-        time.sleep(2)
-        success_placeholder.empty()
+        #Select the Date
+        selected_Date=st.sidebar.date_input(
+            "Select a Date",
+            value=datetime.date.today()-datetime.timedelta(days=1),
+            max_value= datetime.date.today()-datetime.timedelta(days=1)
+        )
+        #Select TimeFrame
+        Start_Time=st.sidebar.time_input(
+            "Provide The Start Time",
+            value=datetime.time(12,0)
+            )
+        Stop_Time=st.sidebar.time_input(
+            "Provide The Stop Time",
+            value=datetime.time(12,0)
+            )
+        if st.sidebar.button("Fetch Data"):
+            #Fetch data from the database where the date is the date of the day and the timeframe is between Start_Time and Stop_Time
+            success_placeholder = st.sidebar.empty()
+            success_placeholder.success("Filters Applied Successfully!")
+            
+            # Wait for 2 seconds before clearing the message
+            time.sleep(2)
+            success_placeholder.empty()
+            
 
 
     #add checkbox
@@ -157,4 +207,15 @@ if selected == "Historical data analysis":
     if st.sidebar.button("Filter"):
         st.sidebar.success("Filtering successfull")
 
+
+    st.sidebar.header("Set Threshold for packet counts")
+    datatime=st.sidebar.slider("Select the Time interval To calulate mean",1,60)
+    pakcetcount=st.sidebar.slider("Select the number of pakcets allowed in this timeframe",1)
+    if st.sidebar.button("Apply The settings"):
+        success_placeholder = st.sidebar.empty()
+        success_placeholder.success("Threshold Applied Successfully!")
+            
+            # Wait for 2 seconds before clearing the message
+        time.sleep(2)
+        success_placeholder.empty()
 
